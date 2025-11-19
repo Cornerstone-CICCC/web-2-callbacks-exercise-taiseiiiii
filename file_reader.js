78577f56c689637d960a8a7bcc78e191cdff3973
taiseiiiii <isemiya.0509@gmail.com>
2025-11-18 20:24:03 -0800

    complete

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,18 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  if (err) throw err;
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    if (err) throw err;
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      if (err) throw err;
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        if (err) throw err;
+        const arr = JSON.parse(data4);
+        const hobbiesStr = arr.join(" and ");
+        const res = `${data1} ${data2} is ${data3} years old and his hobbies are ${hobbiesStr}`;
+        console.log(res);
+      });
+    });
+  });
+});
